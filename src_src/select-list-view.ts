9ee731d5dc41c98b8e2c143b82e017acef12a581
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-01-04 16:26:38 -0600

    JSDoc style doc strings

--- src_src/select-list-view.ts
+++ src_src/select-list-view.ts
@@ -1,7 +1,7 @@
 // TODO: fix redundant returns in methods
 
 const etch = require('etch')
-import {Disposable, CompositeDisposable, TextEditor, CommandEvent } from 'atom'
+import { Disposable, CompositeDisposable, TextEditor, CommandEvent } from 'atom'
 const $ = etch.dom
 import fuzzaldrin from 'fuzzaldrin'
 
@@ -9,89 +9,92 @@ import fuzzaldrin from 'fuzzaldrin'
 type EtchElement = HTMLElement
 
 declare interface SelectListProperties {
-
-  // an array containing the objects you want to show in the select list.
+  /** an array containing the objects you want to show in the select list. */
   items: Array<object | string>
 
-  // a function that is called whenever an item needs to be displayed.
-  //
-  // `options: { selected: boolean, index: number, visible: boolean }`
-  //
-  // - `selected`: indicating whether item is selected or not.
-  // - `index`: item's index.
-  // - `visible`: indicating whether item is visible in viewport or not. Unless initiallyVisibleItemCount was given, this value is always true.
-  elementForItem: (item: object | string, options: { selected: boolean, index: number, visible: boolean }) => EtchElement // TODO: HTMLElement
+  /**
+   * a function that is called whenever an item needs to be displayed.
+   *
+   * `options: { selected: boolean, index: number, visible: boolean }`
+   *
+   * - `selected`: indicating whether item is selected or not.
+   * - `index`: item's index.
+   * - `visible`: indicating whether item is visible in viewport or not. Unless initiallyVisibleItemCount was given, this value is always true.
+   */
+  elementForItem: (
+    item: object | string,
+    options: { selected: boolean; index: number; visible: boolean }
+  ) => EtchElement // TODO: HTMLElement
 
-  // (Optional) the number of maximum items that are shown.
+  /** (Optional) the number of maximum items that are shown. */
   maxResults?: number
 
-  // (Optional) a function that allows to decide which items to show whenever the query changes. By default, it uses fuzzaldrin to filter results.
+  /** (Optional) a function that allows to decide which items to show whenever the query changes. By default, it uses fuzzaldrin to filter results. */
   filter?: (items: Array<object | string>, query: string) => Array<object>
 
-  // (Optional) when filter is not provided, this function will be called to retrieve a string property on each item and that will be used to filter them.
+  /** (Optional) when filter is not provided, this function will be called to retrieve a string property on each item and that will be used to filter them. */
   filterKeyForItem?: (item: object | string) => string
 
-  // (Optional) a function that allows to apply a transformation to the user query and whose return value will be used to filter items.
+  /** (Optional) a function that allows to apply a transformation to the user query and whose return value will be used to filter items. */
   filterQuery?: (query: string) => string
 
-  // (Optional) a string that will replace the contents of the query editor.
+  /** (Optional) a string that will replace the contents of the query editor. */
   query?: string
 
-  // (Optional)  a boolean indicating whether the query text should be selected or not.
+  /** (Optional)  a boolean indicating whether the query text should be selected or not. */
   selectQuery?: boolean
 
-  // (Optional)  a function that allows to change the order in which items are shown.
+  /** (Optional)  a function that allows to change the order in which items are shown. */
   order?: (item1: object | string, item2: object | string) => number
 
-  // (Optional) a string shown when the list is empty.
+  /** (Optional) a string shown when the list is empty. */
   emptyMessage?: string
 
-  // (Optional) a string that needs to be set when you want to notify the user that an error occurred.
+  /** (Optional) a string that needs to be set when you want to notify the user that an error occurred. */
   errorMessage?: string
 
-  // (Optional) a string that needs to be set when you want to provide some information to the user.
+  /** (Optional) a string that needs to be set when you want to provide some information to the user. */
   infoMessage?: string
 
-  // (Optional) a string that needs to be set when you are loading items in the background.
+  /** (Optional) a string that needs to be set when you are loading items in the background. */
   loadingMessage?: string
 
-  // (Optional) a string or number that needs to be set when the progress status changes (e.g. a percentage showing how many items have been loaded so far).
+  /** (Optional) a string or number that needs to be set when the progress status changes (e.g. a percentage showing how many items have been loaded so far). */
   loadingBadge?: string | number
 
-  // (Optional) an array of strings that will be added as class names to the items element.
+  /** (Optional) an array of strings that will be added as class names to the items element. */
   itemsClassList?: Array<string>
 
-  // (Optional) the index of the item to initially select and automatically select after query changes; defaults to 0.
+  /** (Optional) the index of the item to initially select and automatically select after query changes; defaults to 0. */
   initialSelectionIndex?: number
 
-  // (Optional) a function that is called when the query changes.
+  /** (Optional) a function that is called when the query changes. */
   didChangeQuery?: (query: string) => void
 
-  // (Optional) a function that is called when the selected item changes.
+  /** (Optional) a function that is called when the selected item changes. */
   didChangeSelection?: (item: object | string) => void
 
-  // (Optional) a function that is called when the user clicks or presses Enter on an item.
+  /** (Optional) a function that is called when the user clicks or presses Enter on an item. */
   didConfirmSelection?: (item: object | string) => void
 
-  // (Optional) a function that is called when the user presses Enter but the list is empty.
+  /** (Optional) a function that is called when the user presses Enter but the list is empty. */
   didConfirmEmptySelection?: () => void
 
-  // (Optional) a function that is called when the user presses Esc or the list loses focus.
+  /** (Optional) a function that is called when the user presses Esc or the list loses focus. */
   didCancelSelection?: () => void
 
-  // (Optional) When this options was provided, SelectList observe visibility of items in viewport, visibility state is passed as visible option to elementForItem. This is mainly used to skip heavy computation for invisible items.
+  /** (Optional) When this options was provided, SelectList observe visibility of items in viewport, visibility state is passed as visible option to elementForItem. This is mainly used to skip heavy computation for invisible items. */
   initiallyVisibleItemCount?: number
 
   skipCommandsRegistration: boolean
 }
 
 export default class SelectListView {
-
-  // When creating a new instance of a select list, or when calling `update` on an existing one, you can supply an object with the typeof SelectListProperties
+  /** When creating a new instance of a select list, or when calling `update` on an existing one, you can supply an object with the typeof SelectListProperties */
   props: SelectListProperties
 
-  // an array containing the objects you want to show in the select list.
-  items: Array<object| string>  // TODO: Added initializer! Either fix this.items or assign it in constructor
+  /** an array containing the objects you want to show in the select list. */
+  items: Array<object | string> // TODO: Added initializer! Either fix this.items or assign it in constructor
 
   private disposables: CompositeDisposable
   private element: EtchElement
@@ -131,10 +134,12 @@ export default class SelectListView {
     const didLoseFocus = this.didLoseFocus.bind(this)
     editorElement.addEventListener('blur', didLoseFocus)
 
-    // When clicking the scrollbar of the items list, a blur event will be triggered
-    // on the query editor element, but we don't want to treat that as a cancellation.
-    // This mousedown listener allows us to detect this case and restore focus to the
-    // query editor. This is based on https://stackoverflow.com/a/1480178.
+    /**
+     * When clicking the scrollbar of the items list, a blur event will be triggered
+     * on the query editor element, but we don't want to treat that as a cancellation.
+     * This mousedown listener allows us to detect this case and restore focus to the
+     * query editor. This is based on https://stackoverflow.com/a/1480178.
+     */
     this.didClickItemsList = false
     this.element.addEventListener('mousedown', event => {
       if (event.target === this.refs.items) {
@@ -212,7 +217,7 @@ export default class SelectListView {
   }
 
   update (props: SelectListProperties = {}) {
-    // TODO: default value {}
+    /** TODO: default value {} */
 
     let shouldComputeItems = false
 
@@ -237,8 +242,8 @@ export default class SelectListView {
     }
 
     if (props.hasOwnProperty('query')) {
-      // Items will be recomputed as part of the change event handler, so we
-      // don't need to recompute them again at the end of this function.
+      /** Items will be recomputed as part of the change event handler, so we */
+      /** don't need to recompute them again at the end of this function. */
       this.refs.queryEditor.setText(props.query)
       shouldComputeItems = false
     }
